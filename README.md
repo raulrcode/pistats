05936ea12210b2e4797ca5135d4024a8711f7640
raulrcod <118807552+raulrcod@users.noreply.github.com>
2024-01-11 20:34:56 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # pistats
-Check your RPi stats
+
+
+To run the provided shell script, follow these steps:
+
+1. **Clone the repository:**
+```
+git clone https://github.com/raulrcode/pistats.git
+```
+
+2. **Navigate to the script:**
+```
+cd pistats
+```
+
+3. **Make the script Executable:**
+```
+chmod +x pistats.sh
+```
+
+4. **Run the script:**
+```
+./pistats.sh
+```
+
+![example](https://github.com/raulrcode/pistats/assets/118807552/9818557e-b1f4-44ca-a357-ca7f9fee4250)
---
+++ pistats.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# created by https://github.com/raulrcode/
+
+dateFormat="%d.%m.%Y"
+uplinkIpAddress="192.168.1.1"
+cpuUsage=$(top -bn2 | grep '%Cpu' | tail -1 | grep -P '(....|...) id,'|awk '{print "Load: " 100-($8/4) "%"}')
+dateTime=$(date +"$dateFormat | %H:%M:%S")
+cpuFreq=$(vcgencmd measure_clock arm)
+cpuTemp=$(vcgencmd measure_temp | sed 's/temp=/Temp: /')
+wifiQuality=$(iwconfig wlan0 | awk -F= '/Link Quality/{gsub("Signal level=", "", $2); split($2, arr, "/"); printf "%.0f", (arr[1]/arr[2])*100}')
+
+# Extract the clock frequency in Hertz from the output
+clock_frequency_hertz=$(echo "$cpuFreq" | cut -d= -f2)
+
+# Convert Hertz to GHz
+clock_frequency_ghz=$(echo "scale=9; $clock_frequency_hertz / 1000000000" | bc)
+
+# Format the result to display 2 decimal places
+cpuGhz=$(printf "%.2f GHz" "$clock_frequency_ghz")
+
+# Function to check the uplink status
+uplinkStatus=""
+check_uplink() {
+    if ping -c 1 -W 1 "$uplinkIpAddress" > /dev/null 2>&1; then
+        uplinkStatus="Uplink: UP"
+    else
+        uplinkStatus="Uplink: ERROR DOWN"
+    fi
+}
+check_uplink
+
+# Print the stats
+cat <<EOF
+Date: $dateTime
+CPU: $cpuGhz    | $cpuUsage | $cpuTemp
+Wi-Fi Level: $wifiQuality% | $uplinkStatus
+EOF
