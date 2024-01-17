85f662b5816dadb6d057cfcddea304afefbef298
raulrcod <118807552+raulrcod@users.noreply.github.com>
2024-01-17 18:01:37 +0200

    updated commands

--- pistats.sh
+++ pistats.sh
@@ -4,17 +4,26 @@
 dateFormat="%d.%m.%Y"
 timeFormat="%H:%M:%S"
 uplinkIpAddress="192.168.1.1"
-# cpuUsage=$(top -bn2 | grep '%Cpu' | tail -1 | grep -P '(....|...) id,' | awk '{printf "%.2f%%", 100-($8/4)}')
-# cpuThreadUsage="$(ps -eo psr,%cpu | awk '$1 ~ /^[0-9]+$/ {cpu[$1]+=$2} END {for (i in cpu) printf "CPU%d: %.2f%% |", i, cpu[i]}')"
-#                   ps -eo psr,%cpu | awk '$1 ~ /^[0-9]+$/ {cpu[$1]+=$2} END {for (i in cpu) printf "CPU%d: %.2f%% |", i, cpu[i]}'
-cpu_usage_command='ps -eo psr,%cpu | awk '\''$1 ~ /^[0-9]+$/ {cpu[$1]+=$2; count[$1]++} END {total=0; printf "CPU Load: "; for (i in cpu) {total += cpu[i]} printf "%.2f%%\n", total / length(cpu); for (i in cpu) {printf "CPU%d: %.2f%% |", i, cpu[i] / count[i]}}'\'''
-cpuCores=$(nproc)
-#eval $cpu_usage_command
-
 hostName="$(hostname -f)"
+cpuUsageData=$(ps -eo psr,%cpu | awk '
+    $1 ~ /^[0-9]+$/ {
+        cpu[$1] += $2
+        count[$1]++
+    }
+    END {
+        total = 0
+        for (i in cpu) {
+            total += cpu[i]
+            printf "CPU%d: %.2f%% | ", i, cpu[i]
+        }
+        printf "\n%.2f%%", total / length(cpu)
+    }')
+
+cpuAverageUsage=$(echo "$cpuUsageData" | tail -n1)
+cpuCoreUsage=$(echo "$cpuUsageData" | sed '$d')
 dateTime=$(date +"$dateFormat | $timeFormat")
 cpuFreq=$(vcgencmd measure_clock arm)
-cpuTemp=$(vcgencmd measure_temp | sed 's/temp=/Temp: /')
+cpuTemp=$(vcgencmd measure_temp | sed 's/temp=//')
 wifiQuality=$(iwconfig wlan0 | awk -F= '/Link Quality/{gsub("Signal level=", "", $2); split($2, arr, "/"); printf "%.0f", (arr[1]/arr[2])*100}')
 totalRam="$(free -m | awk 'NR==2 {print $2}')"
 usedRam="$(free -m | awk 'NR==2 {print $3}')"
@@ -37,15 +46,13 @@ check_uplink() {
     fi
 }
 check_uplink
-eval $cpu_usage_command
 
 # Print the stats
 cat <<EOF
 Hostname: $hostName
 Date: $dateTime
-CPU: $cpuGhz    | Cores: $cpuCores | $cpuTemp
-CPU Load: $(eval $cpu_usage_command)
-# $cpuThreadUsage
+CPU: $cpuGhz    | Load: $cpuAverageUsage | Temp: $cpuTemp
+$cpuCoreUsage
 RAM: Used $usedRam MB | Total $totalRam MB
 Wi-Fi Level: $wifiQuality% | $uplinkStatus
 EOF
